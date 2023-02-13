092557ff109c36d9dde015199856e87a86fc4a18
aj124578 <aj124578@naver.com>
2023-02-13 20:22:21 +0900

    ProductRepository에 insert() 이동 완료

---
+++ app/src/main/java/myjavadb/model/ProductRepository.java
@@ -0,0 +1,35 @@
+package myjavadb.model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ProductRepository {
+        private Connection conn;
+
+        private void insert(String name, int price, int qty) throws SQLException {
+
+            // 2. 버퍼 접근
+            String sql = "insert into product(name, price, qty, created_at) values(?,?,?,now())";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+
+            // 3. 물음표 완성
+            pstmt.setString(1, name);
+            pstmt.setInt(2, price);
+            pstmt.setInt(3, qty);
+
+            // 4. 전송
+            int result = pstmt.executeUpdate();
+
+            // 5. 응답에 대한 처리
+            if (result == 1) {
+                System.out.println("insert 되었습니다.");
+            } else {
+                System.out.println("insert 실패");
+            }
+
+            // 6. 최종 마무리
+
+            pstmt.close();
+        }
+    }
