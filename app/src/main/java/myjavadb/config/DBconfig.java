a9045e1eade1868f2d798eba2e7d70a80f56776f
aj124578 <aj124578@naver.com>
2023-02-13 17:22:41 +0900

    MySQL JDBC 연결 완료

--- app/src/main/java/myjavadb/config/DBconfig.java
+++ app/src/main/java/myjavadb/config/DBconfig.java
@@ -8,17 +8,13 @@ public class DBconfig {
     public static Connection getConnection(){
         
         try {
-            Class.forName("com.mysql.cj.jdbc.Drvier");
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhsot:3306/metadb", "root", "1234");
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/metadb", "root", "1234");
             System.out.println("DB연결 성공");
             return conn;
         } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
-    }
-
-    public static void main(String[] args) {
-        DBconfig.getConnection();
     }
 }
